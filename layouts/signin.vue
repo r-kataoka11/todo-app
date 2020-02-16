4fed10eaea6964903a7e14c34c715b0aefd0f44c
Ryuichi Kataoka <r.kataoka.ps.ai@gmail.com>
2020-02-16 15:19:52 +0900

    サインインページレイアウト調整

---
+++ layouts/signin.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-app dark>
+    <v-content>
+      <v-container fluid fill-height>
+        <nuxt />
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
--- pages/signin.vue
+++ pages/signin.vue
@@ -1,3 +1,21 @@
 <template>
-  <amplify-authenticator />
+  <v-layout
+    column
+    justify-center
+    align-center
+  >
+    <v-flex
+      xs12
+      sm8
+      md6
+    >
+      <amplify-authenticator />
+    </v-flex>
+  </v-layout>
 </template>
+
+<script>
+export default {
+  layout: 'signin'
+}
+</script>
