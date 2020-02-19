cf9c31d46b4d83d9748c8e67dafc498b0013d273
Ryuichi Kataoka <r.kataoka.ps.ai@gmail.com>
2020-02-19 21:03:34 +0900

    API部分実装

---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,39 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateTodo = /* GraphQL */ `
+  subscription OnCreateTodo {
+    onCreateTodo {
+      taskid
+      title
+      content
+      is_completed
+      created_at
+      updated_at
+    }
+  }
+`;
+export const onUpdateTodo = /* GraphQL */ `
+  subscription OnUpdateTodo {
+    onUpdateTodo {
+      taskid
+      title
+      content
+      is_completed
+      created_at
+      updated_at
+    }
+  }
+`;
+export const onDeleteTodo = /* GraphQL */ `
+  subscription OnDeleteTodo {
+    onDeleteTodo {
+      taskid
+      title
+      content
+      is_completed
+      created_at
+      updated_at
+    }
+  }
+`;
