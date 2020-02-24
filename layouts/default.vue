192618f6b268503c4c03d2d3be3315145cd69549
Ryuichi Kataoka <r.kataoka.ps.ai@gmail.com>
2020-02-25 01:29:42 +0900

    不要なファイルを削除

--- layouts/default.vue
+++ layouts/default.vue
@@ -22,12 +22,6 @@
         <nuxt />
       </v-container>
     </v-content>
-    <v-footer
-      :fixed="fixed"
-      app
-    >
-      <span>&copy; 2019</span>
-    </v-footer>
   </v-app>
 </template>
 
